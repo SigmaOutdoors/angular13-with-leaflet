94396130bb265572035561b1df142bcc2c68af17
SigmaOutdoors <daryl@sigmaoutdoors.com>
2023-01-20 15:30:34 -0500

    Lines

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,6 @@ import { AfterViewInit, Component } from '@angular/core';
 import { Observable, Subscriber } from 'rxjs';
 import * as L from 'leaflet';
 
-
-
 import { environment } from '../environments/environment';
 import { HelperService } from './helper.service';
 
@@ -11,21 +9,27 @@ import { HelperService } from './helper.service';
  * DESC:
  * The initial reason I wrote this code it to figure out how to present WMS layers (not overlay image layers)
  * on a leaflet map and have the ORDERS of those layers (z-index if you will) be different from the presented
- * order in the GUI.  ASAIK, the order they appear (z-index) is the order they get created in with the 
+ * order in the GUI.  ASAIK, the order they appear (z-index) is the order they get created in with the
  * LAST being the TOPMOST layer. So maybe you want something more imporant in ORDER being
- * HIGHER on the presentation list?  How does that happen?  Well this project was an 
- * experiment in that. 
- * 
+ * HIGHER on the presentation list?  How does that happen?  Well this project was an
+ * experiment in that.
+ *
  * UPDATE!!!
  * I ended up finding in discovery (mostly after the fact) that PANES can be used to achieve this.
- * I am leaving this project HERE with the custom control  
- * 
- * 
- * MISC: 
+ * I am leaving this project HERE with the custom control
+ *
+ *
+ * MISC:
  * Took out the code to get your location and hard-coded a lat/long
  * Made the fly to marker code independent of load so map isn't constantly
  * flying to a location as you type in stackblitz
  */
+
+let data = [
+  { latitude: 37.788022, longitude: -122.399797 },
+  { latitude: 40.748817, longitude: -73.985428 },
+  { latitude: 34.052235, longitude: -118.243683 },
+];
 
 @Component({
   selector: 'app-root',
@@ -36,59 +40,92 @@ export class AppComponent implements AfterViewInit {
   map: L.Map;
 
   /**
-   * 
+   *
    * To see the custom leaflet control, set this to false
-   * 
+   *
    */
   enableStandardLayerGroupControl = true;
-
-
 
   lat = 26.3398;
   lng = -81.7787;
   center = L.latLng(this.lat, this.lng); // [this.lat, this.lng];
-  message = "";
+  message = '';
   overlayCollection = [];
-  builtInLayerGroupControlOrder : L.Control.LayersObject ;
-  builtInLayerGroupControlOrderAlt : L.Control.LayersObject ;
+  builtInLayerGroupControlOrder: L.Control.LayersObject;
+  builtInLayerGroupControlOrderAlt: L.Control.LayersObject;
   customOverlayOrder = [];
   mapCollection; //  = new Map();
   baseMaps;
   layerControl;
   streetsBaseMap;
-  position: L.ControlPosition = "bottomleft" ;
-
- 
-
+  position: L.ControlPosition = 'bottomleft';
 
   mbAttr =
-  'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
-  mbUrl = 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + environment.mapbox.accessToken;
+    'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>';
+  mbUrl =
+    'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' +
+    environment.mapbox.accessToken;
+
+  constructor(private helperService: HelperService) {}
+
+  private drawLinesAndPointsAsFeatureGroup() {
+    var destinationHTML = document.getElementById('destinations-body');
+
+    var fg = L.featureGroup().addTo(this.map);
+    var destinations = [];
+
+    var line = L.polyline([]).addTo(this.map);
+    for (var row in data) {
+      var marker = L.marker([data[row].latitude, data[row].longitude]).addTo(
+        fg
+      );
+      line.addLatLng(marker.getLatLng());
+    }
+  }
 
 
-  constructor(private helperService : HelperService) {
-    
+  //     this.mapCollection.set(
+   // 'cities',
+  //  L.layerGroup([littleton, bloomfield, aurora, bonita], {
+  //    pane: 'TOPMOST1',
+  //  })
+  //);
+  private drawLinesAndPointsAsLayerGroup() {
+
+    var lg = L.layerGroup(); // .addTo(this.map);
+    var destinations = [];
+
+    var line = L.polyline([]).addTo(lg);
+    for (var row in data) {
+      var marker = L.marker([data[row].latitude, data[row].longitude], { pane: 'TOPMOST1' }).addTo(
+        lg
+      );
+      line.addLatLng(marker.getLatLng());
+    }
+
+    this.mapCollection.set(
+      'lax',
+ lg
+      
+    );
+
+
 
 
   }
 
   public ngAfterViewInit(): void {
-
     // Go get the collection data (the real order of the layers from serivce)
-    this.helperService.getMapData().subscribe( r => {
-        this.mapCollection = r;
+    this.helperService.getMapData().subscribe((r) => {
+      this.mapCollection = r;
     });
 
     this.createCustomLayerOrder();
     this.loadMap();
-   
-    setTimeout(() => {
-     
-    }, 3000);
+   // this.drawLinesAndPointsAsFeatureGroup();
 
+    setTimeout(() => {}, 3000);
   }
-
- 
 
   /**
    * Fly to a marker on the map
@@ -104,17 +141,17 @@ export class AppComponent implements AfterViewInit {
       popupAnchor: [13, 0],
     });
 
-    const marker = L.marker(this.center, { icon }).bindPopup('Sunny Bonita Springs');
+    const marker = L.marker(this.center, { icon }).bindPopup(
+      'Sunny Bonita Springs'
+    );
     marker.addTo(this.map);
-  
   }
 
   private loadMap(): void {
     this.map = L.map('map').setView(this.center, 4);
 
-
     // UPDATE, I ended up finding out assigning panes to thw WMS layers
-    // with approriate z-Index will do the ordering you want on the default 
+    // with approriate z-Index will do the ordering you want on the default
     // control without having to force them into a particular order on the default
     // Group Layer control.
 
@@ -141,65 +178,61 @@ export class AppComponent implements AfterViewInit {
     this.map.createPane('TOPMOST1');
     this.map.getPane('TOPMOST1').style.zIndex = '9500';
 
-    this.streetsBaseMap = L.tileLayer(
-      this.mbUrl,
-      {
-        attribution: this.mbAttr,
-        maxZoom: 18,
-        id: 'mapbox/streets-v11',
-        tileSize: 512,
-        zoomOffset: -1,
-        accessToken: environment.mapbox.accessToken,
-      }
-    ).addTo(this.map);
+    this.streetsBaseMap = L.tileLayer(this.mbUrl, {
+      attribution: this.mbAttr,
+      maxZoom: 18,
+      id: 'mapbox/streets-v11',
+      tileSize: 512,
+      zoomOffset: -1,
+      accessToken: environment.mapbox.accessToken,
+    }).addTo(this.map);
 
-    
     /**
-    * You want to disable the DEFAULT LAYER GROUP control to effectively use the Custom Control.
-    */
-    
-     if (this.enableStandardLayerGroupControl) {
-        this.message = "With the default layer control enabled, the custom layer control will not work as expected, to demo custom control properly, disable the default control in code."
-        this.createDefaultLayerControl();
-     }
+     * You want to disable the DEFAULT LAYER GROUP control to effectively use the Custom Control.
+     */
 
-  }
-
-  public createCustomLayerOrder()
-  {
-
-
-    this.customOverlayOrder = [    {
-      title: 'Cables',
-      checked: false,
-    },
-    {
-      title: 'NOAA1',
-      checked: false,
-    },
-    {
-      title: 'Topo (Opacity 1)',
-      checked: false,
-    },
-    {
-      title: 'Topo (Opacity .4)',
-      checked: false,
+    if (this.enableStandardLayerGroupControl) {
+      this.message =
+        'With the default layer control enabled, the custom layer control will not work as expected, to demo custom control properly, disable the default control in code.';
+      this.createDefaultLayerControl();
     }
-
-  ];
-
   }
 
+  public createCustomLayerOrder() {
+    this.customOverlayOrder = [
+      {
+        title: 'Cables',
+        checked: false,
+      },
+      {
+        title: 'NOAA1',
+        checked: false,
+      },
+      {
+        title: 'Topo (Opacity 1)',
+        checked: false,
+      },
+      {
+        title: 'Topo (Opacity .4)',
+        checked: false,
+      },
+    ];
+  }
 
   public createDefaultLayerControl() {
-
     // 42.8973° N, 77.4214° W
     var littleton = L.marker([39.61, -105.02]).bindPopup(
         'This is Littleton, CO.'
       ),
-      bloomfield = L.marker([42.8973, -77.4214], { pane: "TOPMOST1" }).bindPopup('This is Denver, CO.'),
-      aurora = L.marker([39.73, -104.8], { pane: "TOPMOST1" }).bindPopup('This is Aurora, CO.'),
-      bonita = L.marker(this.center,{ pane: "TOPMOST1" }).bindPopup('This is Bonita Springs, Fl.');
+      bloomfield = L.marker([42.8973, -77.4214], {
+        pane: 'TOPMOST1',
+      }).bindPopup('This is Denver, CO.'),
+      aurora = L.marker([39.73, -104.8], { pane: 'TOPMOST1' }).bindPopup(
+        'This is Aurora, CO.'
+      ),
+      bonita = L.marker(this.center, { pane: 'TOPMOST1' }).bindPopup(
+        'This is Bonita Springs, Fl.'
+      );
 
     var grayScaleBaseMap = L.tileLayer(this.mbUrl, {
       id: 'mapbox/light-v9',
@@ -210,8 +243,15 @@ export class AppComponent implements AfterViewInit {
 
     // !!!! DOESN'T SEEM TO WORK FOR cities even with pane option, gets covered by TOPO (Opacity 1)
     // In order for this to work you need to do it at the MARKER level (see code above)
-    // This pane option is DOING NOTING 
-    this.mapCollection.set('cities', L.layerGroup([littleton, bloomfield, aurora, bonita], {  pane : 'TOPMOST1'}));
+    // This pane option is DOING NOTING
+    this.mapCollection.set(
+      'cities',
+      L.layerGroup([littleton, bloomfield, aurora, bonita], {
+        pane: 'TOPMOST1',
+      })
+    );
+
+    this.drawLinesAndPointsAsLayerGroup();
 
     this.baseMaps = {
       Grayscale: grayScaleBaseMap,
@@ -227,31 +267,27 @@ export class AppComponent implements AfterViewInit {
       NOAA1: this.mapCollection.get('NOAA1'),
       'Topo (Opacity 1)': this.mapCollection.get('Topo (Opacity 1)'),
       'Topo (Opacity .4)': this.mapCollection.get('Topo (Opacity .4)'),
-     
-     
-      
+      'lax': this.mapCollection.get('lax'),
     };
 
-    
-   // an alternate order to test with and show what happens 
-   // In this case Topo (Opacity 1) will be "under" NOAA1
-   this.builtInLayerGroupControlOrderAlt = {
-      Cities: this.mapCollection.get('cities'),     
+    // an alternate order to test with and show what happens
+    // In this case Topo (Opacity 1) will be "under" NOAA1
+    this.builtInLayerGroupControlOrderAlt = {
+      Cities: this.mapCollection.get('cities'),
       'Topo (Opacity 1)': this.mapCollection.get('Topo (Opacity 1)'),
       'Topo (Opacity.4)': this.mapCollection.get('Topo (Opacity .4)'),
       'Cables 2nd': this.mapCollection.get('Cables'),
-      NOAA1: this.mapCollection.get('NOAA1')
-      
-    }; 
+      NOAA1: this.mapCollection.get('NOAA1'),
+    };
 
-     // You could do base and layer separate
-     // L.control.layers(this.baseMaps, null).addTo(this.map);
-     // this.layerControl = L.control.layers(null, this.overlayMaps).addTo(this.map);
-     // Or add them both to same control 
+    // You could do base and layer separate
+    // L.control.layers(this.baseMaps, null).addTo(this.map);
+    // this.layerControl = L.control.layers(null, this.overlayMaps).addTo(this.map);
+    // Or add them both to same control
 
-     this.layerControl = L.control.layers(this.baseMaps, this.builtInLayerGroupControlOrder).addTo(this.map);
-
-       
+    this.layerControl = L.control
+      .layers(this.baseMaps, this.builtInLayerGroupControlOrder)
+      .addTo(this.map);
   }
 
   /**
@@ -265,8 +301,7 @@ export class AppComponent implements AfterViewInit {
     let imageBounds = L.latLngBounds(this.center, L.latLng(-35.865, 154.2094));
 
     // don't keep adding it id they keep whacking the button
-    if(this.isActiveLayer(this.mapCollection.get('overlay1')))
-       return;
+    if (this.isActiveLayer(this.mapCollection.get('overlay1'))) return;
 
     this.mapCollection.set(
       'overlay1',
@@ -278,76 +313,67 @@ export class AppComponent implements AfterViewInit {
   }
 
   public activeLayers() {
-    this.map.eachLayer(ll => {
-       console.log(ll);
+    this.map.eachLayer((ll) => {
+      console.log(ll);
     });
   }
 
-  public isActiveLayer(layerValue) : boolean {
+  public isActiveLayer(layerValue): boolean {
     let result = false;
-    this.map.eachLayer(ll => {  
-       if ( ll == layerValue)
-        result=true;
+    this.map.eachLayer((ll) => {
+      if (ll == layerValue) result = true;
     });
     return result;
   }
 
-  private inMapCollection(vv) : boolean
-  {
+  private inMapCollection(vv): boolean {
     for (let [key, value] of this.mapCollection) {
       if (vv == value) {
-          console.log("In the mappcollection!");
-          return true;
+        console.log('In the mappcollection!');
+        return true;
       }
     }
     return false;
   }
 
   /**
-   * Remove layers by default 
+   * Remove layers by default
    */
   public dumpMapCollectionAndRemoveLayers(removeLayers = true) {
-
     for (let [key, value] of this.mapCollection) {
       console.log(key);
-      console.log(value)
-      if (removeLayers)
-          this.map.removeLayer(value);
+      console.log(value);
+      if (removeLayers) this.map.removeLayer(value);
     }
   }
-
 
   /**
    * This just swaps out the orginal control for my #2 control with a different order.
    */
-  public newLayerControl()
-  {
-
-   this.map.removeControl(this.layerControl);
-   this.dumpMapCollectionAndRemoveLayers(true);
-   this.layerControl = L.control.layers(this.baseMaps, this.builtInLayerGroupControlOrderAlt).addTo(this.map);
-   
+  public newLayerControl() {
+    this.map.removeControl(this.layerControl);
+    this.dumpMapCollectionAndRemoveLayers(true);
+    this.layerControl = L.control
+      .layers(this.baseMaps, this.builtInLayerGroupControlOrderAlt)
+      .addTo(this.map);
   }
 
-   /**
+  /**
    * Not used ATM, but kept for informational purposes.
    */
-    private getCurrentPosition(): any {
-      return new Observable((observer: Subscriber<any>) => {
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition((position: any) => {
-            observer.next({
-              latitude: position.coords.latitude,
-              longitude: position.coords.longitude,
-            });
-            observer.complete();
+  private getCurrentPosition(): any {
+    return new Observable((observer: Subscriber<any>) => {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position: any) => {
+          observer.next({
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude,
           });
-        } else {
-          observer.error();
-        }
-      });
-    }
-
-
-
+          observer.complete();
+        });
+      } else {
+        observer.error();
+      }
+    });
+  }
 }
